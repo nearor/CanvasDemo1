756bd6453ea481c0b23109de20533030d659c005
nearor <linkinzhk@gmail.com>
2016-05-12 17:44:27 +0800

    完成canvas绘画一个仪表盘

--- instrument/src/main/java/com/example/instrument/MyView.java
+++ instrument/src/main/java/com/example/instrument/MyView.java
@@ -10,6 +10,74 @@ import android.view.View;
  * Created by Nearor on 5/9/16.
  */
 public class MyView extends View {
+
+    private int mWight ;
+
+    private int mHeight;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //获取宽高参数
+        mWight = getMeasuredWidth();
+
+        mHeight = getMeasuredHeight();
+
+        //画外圆
+        Paint paintCircle = new Paint();
+        paintCircle.setStyle(Paint.Style.STROKE);
+        paintCircle.setAntiAlias(true);
+        paintCircle.setStrokeWidth(5);
+        canvas.drawCircle(mWight / 2,mHeight / 2,mWight / 2,paintCircle);
+
+        //画刻度
+        Paint paintLine = new Paint();
+        paintCircle.setStrokeWidth(3);
+        for (int i = 0; i < 24; i++) {
+            if(i==0||i==6||i==12||i==18){
+                paintLine.setStrokeWidth(5);
+                paintLine.setTextSize(30);
+                canvas.drawLine(mWight / 2,mHeight / 2 - mWight / 2,
+                        mWight / 2,mHeight / 2 - mWight / 2 + 60,
+                        paintLine);
+                String degree = String.valueOf(i);
+                canvas.drawText(degree,
+                        mWight / 2 - paintLine.measureText(degree) / 2,
+                        mHeight / 2 - mWight / 2 + 90,
+                        paintLine);
+            }else {
+                paintLine.setStrokeWidth(3);
+                paintLine.setTextSize(15);
+                canvas.drawLine(mWight / 2, mHeight / 2 - mWight / 2,
+                        mWight / 2,mHeight / 2 - mWight / 2 + 30,
+                        paintLine);
+                String degree = String.valueOf(i);
+                canvas.drawText(degree,mWight / 2 - paintLine.measureText(degree) / 2,
+                        mHeight / 2 - mWight / 2 + 60,
+                        paintLine);
+            }
+
+            canvas.rotate(15,mWight / 2,mHeight / 2);
+        }
+
+       // 画圆心
+        Paint paintPoint = new Paint();
+        paintPoint.setStrokeWidth(30);
+        canvas.drawPoint(mWight / 2,mHeight / 2,paintPoint);
+
+        //画指针
+        Paint paintHour = new Paint();
+        paintHour.setStrokeWidth(20);
+        Paint paintMinute = new Paint();
+        paintMinute.setStrokeWidth(10);
+        canvas.save();
+        canvas.translate(mWight / 2,mHeight / 2);
+        canvas.drawLine(0,0,100,100,paintHour);
+        canvas.drawLine(0,0,100,200,paintMinute);
+        canvas.restore();
+    }
+
     public MyView(Context context) {
         super(context);
     }
@@ -23,28 +91,4 @@ public class MyView extends View {
     }
 
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        //画外圆
-
-        Paint paintCircle = new Paint();
-        paintCircle.setStyle(Paint.Style.STROKE);
-        paintCircle.setAntiAlias(true);
-        paintCircle.setStrokeWidth(5);
-        canvas.drawCircle(400,400,400,paintCircle);
-
-        //画刻度
-        Paint paintLine = new Paint();
-        for (int i = 0; i < 24; i++) {
-            if(i==0||i==6||i==12||i==18){
-                paintLine.setStrokeWidth(5);
-                paintLine.setTextSize(30);
-                canvas.drawLine(400,0,400,0,paintLine);
-
-            }
-        }
-
-    }
 }
